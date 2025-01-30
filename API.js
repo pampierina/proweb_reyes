668bbf0c06db7037087336532b99f174f0a34ed7
pampierina <167271001+pampierina@users.noreply.github.com>
2025-01-30 18:40:22 -0400

    corregir error
    por alguna razon se ve bien en telefono pero no en pc? aqui areglandolo

--- API.js
+++ API.js
@@ -1,32 +1,33 @@
 async function fetchPorygon() {
-    try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/porygon');
-        const data = await response.json();
+  try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon/porygon');
+      const data = await response.json();
 
-        // Mostrar la información en la página
-        document.getElementById('sprite').src = data.sprites.front_shiny;
-        document.getElementById('nombre').innerText = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-        document.getElementById('numero').innerText = data.id;
-        document.getElementById('tipo').innerText = data.types.map(typeInfo => typeInfo.type.name).join(', ').charAt(0).toUpperCase() + data.types.map(typeInfo => typeInfo.type.name).join(', ').slice(1);
-    } catch (error) {
-        console.error('Error al obtener la información de Porygon:', error);
-    }
+      // Mostrar la información en la página
+      document.getElementById('sprite').src = data.sprites.front_shiny;
+      document.getElementById('nombre').innerText = data.name.charAt(0).toUpperCase() + data.name.slice(1);
+      document.getElementById('numero').innerText = data.id;
+      document.getElementById('tipo').innerText = data.types.map(typeInfo => typeInfo.type.name).join(', ').charAt(0).toUpperCase() + data.types.map(typeInfo => typeInfo.type.name).join(', ').slice(1);
+  } catch (error) {
+      console.error('Error al obtener la información de Porygon:', error);
+  }
 }
-
-
-window.onload = fetchPorygon;
-
-// 2da API de gatos
-const catImg = document.getElementById('cat-img');
-
 
 async function getRandomCat() {
-  const response = await fetch("https://api.thecatapi.com/v1/images/search");
-  const data = await response.json();
-  return data[0];
+  try {
+      const response = await fetch("https://api.thecatapi.com/v1/images/search");
+      const data = await response.json();
+
+      // Mostrar la imagen de gato en la página
+      document.getElementById('cat-img').src = data[0].url;
+  } catch (error) {
+      console.error('Error al obtener la imagen de gato:', error);
+  }
 }
 
-async function handleClick() {
-  const img = await getRandomCat();
-  catImg.src = img.url;
+function handleClick() {
+  getRandomCat();
 }
+
+// Llamar a la función para obtener la información de Porygon al cargar la página
+window.onload = fetchPorygon;
